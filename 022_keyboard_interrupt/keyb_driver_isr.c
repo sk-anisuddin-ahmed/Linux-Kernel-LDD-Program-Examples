6c5e1e7e61e8a26658ff4260676af87187afaf55
Sk Anisuddin Ahmed <skanisuddinahmed@gmail.com>
2026-02-09 13:04:24 +0530

    update

--- 022_keyboard_interrupt/keyb_driver_isr.c
+++ 022_keyboard_interrupt/keyb_driver_isr.c
@@ -5,7 +5,9 @@
 #include <linux/workqueue.h>
 #include <asm/io.h>
 
-#define KEYBOARD_IRQ	0x1
+// Checking in AM335x SOC - Beagle Bone Black
+// 39:	1	INTC	19	Level	musb-hdrc.1
+#define KEYBOARD_IRQ	39
 
 static struct work_struct kb_work;
 
@@ -28,7 +30,7 @@ static int __init keyboard_driver_init(void)
                     keyboard_irq_handler,
                     IRQF_SHARED,
                     "keyb_snif",
-                    NULL)) 
+                    &kb_work)) 
 	{
         printk(KERN_ERR "Failed to register keyboard IRQ\n");
         return -1;
@@ -41,7 +43,6 @@ static int __init keyboard_driver_init(void)
 static void __exit keyboard_driver_exit(void)
 {
     free_irq(KEYBOARD_IRQ, (void *)(keyboard_irq_handler));
-    flush_scheduled_work(&kb_work);
     printk(KERN_INFO "Keyboard sniffer unloaded\n");
 }
 
@@ -49,3 +50,4 @@ module_init(keyboard_driver_init);
 module_exit(keyboard_driver_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anis");
